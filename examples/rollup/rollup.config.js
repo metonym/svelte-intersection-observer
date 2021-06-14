01aeb89edd97d16b6d6b999b957d1f8e5c3e6d80
Eric Liu <ericyl.us@gmail.com>
2021-06-14 13:45:39 -0700

    chore(examples): add rollup example

---
+++ examples/rollup/rollup.config.js
@@ -0,0 +1,25 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "src/index.js",
+  output: {
+    sourcemap: !production,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
+  },
+  plugins: [
+    svelte({
+      emitCss: false,
+      compilerOptions: { dev: !production },
+    }),
+    resolve({ browser: true, dedupe: ["svelte"] }),
+    commonjs(),
+    production && terser(),
+  ],
+};
