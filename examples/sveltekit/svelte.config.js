983b3e4ee5e2ef0613eb0c60b9cbf9fb8a92a932
metonym <ericyl.us@gmail.com>
2022-03-30 07:14:44 -0700

    chore(deps-dev): bump deps

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,9 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
-  },
+  kit: {},
 };
 
 export default config;
