cb9ad7b77b9b0631310ac77592a03092d0e1b218
Eric Liu <ericyl.us@gmail.com>
2024-01-01 15:02:25 -0800

    test: fix package resolution

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
       plugins: [svelte({ preprocess: vitePreprocess() })],
       resolve: {
         alias: {
-          [pkg.name]: pkg.main,
+          [pkg.name]: "./src/index.js",
         },
       },
     },
