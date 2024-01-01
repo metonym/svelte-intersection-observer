edc5c0418753e719f0bdd8091f9d95ea238782e5
Eric Liu <ericyl.us@gmail.com>
2024-01-01 14:39:08 -0800

    build: customize package process

---
+++ scripts/npm-package.js
@@ -0,0 +1,34 @@
+// @ts-check
+
+import fs from "node:fs";
+import fsp from "node:fs/promises";
+
+(async () => {
+  console.time("package");
+
+  if (fs.existsSync("./package")) {
+    await fsp.rm("./package", { recursive: true });
+  }
+
+  await fsp.mkdir("./package");
+
+  await fsp.cp("./src", "./package", { recursive: true });
+  await fsp.copyFile("./package.json", "./package/package.json");
+  await fsp.copyFile("./README.md", "./package/README.md");
+  await fsp.copyFile("./LICENSE", "./package/LICENSE");
+
+  const pkgJson = JSON.parse(
+    fs.readFileSync("./package/package.json", "utf-8"),
+  );
+
+  delete pkgJson.scripts;
+  delete pkgJson.devDependencies;
+  delete pkgJson.prettier;
+
+  await fsp.writeFile(
+    "./package/package.json",
+    JSON.stringify(pkgJson, null, 2),
+  );
+
+  console.timeEnd("package");
+})();
