cff8675bb8157d975cddaa9631fc0c9c6ba650c5
Eric Liu <ericyl.us@gmail.com>
2026-07-04 17:40:59 -0700

    chore(docs): use custom fonts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     svelteReadme({
+      head: `
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&family=Public+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      `,
       style: `
+        :root {
+          --sr-font-sans: "Public Sans", system-ui, sans-serif;
+          --sr-font-mono: "JetBrains Mono", ui-monospace, monospace;
+        }
+
         .code-fence {
           display: flex;
           flex-direction: column;
